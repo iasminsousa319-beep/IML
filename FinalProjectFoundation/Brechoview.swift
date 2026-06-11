8757560632796b984abbd8ca06d037a4e47dab9c
iasminsousa319-beep <iasminsousa319@gmail.com>
2026-06-11 09:59:12 -0300

    salvar3

--- FinalProjectFoundation/Brechoview.swift
+++ FinalProjectFoundation/Brechoview.swift
@@ -7,10 +7,37 @@
 import SwiftUI
 
 struct Brechoview: View {
+
+    let exemplo = Brecho(
+        nome: "Brechó da Vila",
+        endereco: "Benfica",
+        descricao: "Roupas seminovas e vintage",
+        imagem: UIImage(resource: .benchó).pngData()
+    )
+
+    let colunas = [
+        GridItem(.flexible()),
+        GridItem(.flexible())
+    ]
+
     var body: some View {
-       NavigationStack {
-            Text("")
-               .navigationTitle("Brechós")
+        NavigationStack {
+
+            ScrollView {
+
+                LazyVGrid(columns: colunas, spacing: 20) {
+
+                    BrechoCardViewTela(brecho: exemplo)
+                    BrechoCardViewTela(brecho: exemplo)
+                    BrechoCardViewTela(brecho: exemplo)
+                    BrechoCardViewTela(brecho: exemplo)
+                    BrechoCardViewTela(brecho: exemplo)
+                    BrechoCardViewTela(brecho: exemplo)
+
+                }
+                .padding()
+            }
+            .navigationTitle("Brechós")
         }
     }
 }
